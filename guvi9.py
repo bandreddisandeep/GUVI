aa1edd40c27c1b1a884d920295547b9839df37f4
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-20 23:11:30 +0530

    Update guvi9.py

--- guvi9.py
+++ guvi9.py
@@ -2,7 +2,10 @@ x=input().split()
 i=int(x[0])
 a=int(x[1])
 suma=0
-y=input().split()
-for j in range(a):
-    suma+=int(y[j])
-print(suma)
+if i>=a:
+    y=input().split()
+    for j in range(a):
+        suma+=int(y[j])
+    print(suma)
+else:
+    print('invalid')
