ffcc84fa23ded99f6f5b331f6d48281b3d30178d
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-21 22:51:33 +0530

    Update Discarding common length.py

--- Discarding common length.py
+++ Discarding common length.py
@@ -10,8 +10,11 @@ else:
     b=y
 
 for i in range(len(a)):
-    k=j=0
-    if a[i]!=b[i]:
+    if a[i] in ['1','2','3','4','5','6','7','8','9','0']:
+        if a[i] in b:
+            b.remove(a[i])
+    
+    elif a[i]!=b[i]:
         break
     else:
         c.append(a[i])
