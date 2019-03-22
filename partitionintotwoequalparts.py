fdc84f34060fa08c66de5a7cb954cc02d4ab6c97
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-22 19:22:03 +0530

    Update partitionintotwoequalparts.py

--- partitionintotwoequalparts.py
+++ partitionintotwoequalparts.py
@@ -3,16 +3,14 @@ b=int(a[0])
 c=int(a[1])
 d=int(a[2])
 if b%2==0:
-    b=b//2
-    c1=b//c
-    d1=b//d
+    b1=b//2
+    c1=c+d
+    d1=b//c1
     
     f=0
-    for i in range(c1+1):
-        for j in range(d1+1):
-           
-            if (c*i+d*j)==b:
-                f=1
+    for i in range(d1+1):
+        if (c*i+d*i)==b1:
+            f=1
     if f==0:
         print('NO')
     else:
