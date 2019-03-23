75222a61dabab79a8faecf98fdd1a54d269f04f1
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-23 11:46:34 +0530

    Update commonindexes.py

--- commonindexes.py
+++ commonindexes.py
@@ -1,10 +1,17 @@
 n=int(input())
 a=input().split()
 a1=[]
+a2=[]
 for i in range(n):
     a1.append(int(a[i]))
 
 for j in range(n):
     if j==a1[j]:
-        print(j , end=" ")
+        a2.append(j)
+if len(a2)!=0:
+    for k in range(len(a2)):
+        print(a2[k], end=" ")
+else:
+    print(-1)
+        
 
