ca510e0d85069ff4edaa2628bfd078a385612a15
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-23 17:16:56 +0530

    Update space.py

--- space.py
+++ space.py
@@ -5,7 +5,13 @@ s=[]
 for i in range(a+1,b):
     if i%2==0:
         s.append(i)
-for j in range(len(s)-1):
-    print(s[j],end=" ")
-print(s[-1],end="")
+        
+if len(s)==1:
+    print(s[0])
+else:
+        
+    for j in range(0,len(s)-1):
+        print(s[j],end=" ")
+    print(s[-1],end="")
+
 
