2f5848c023e5110fe078ec07af49b95080bf48dc
bandreddisandeep <45593612+bandreddisandeep@users.noreply.github.com>
2019-03-27 23:01:00 +0530

    Update checksquare.py

--- checksquare.py
+++ checksquare.py
@@ -4,7 +4,7 @@ c=input().split()
 d=input().split()
 x=int(b[1])-int(a[1])
 
-if int(b[0])-int(a[0])==0 and int(c[0])-int(b[0])==x and int(c[1])-int(b[1])==0 and int(d[0])-int(c[0])==0 and int(c[1])-int(d[1])==x :
+if x>0 and int(b[0])-int(a[0])==0 and int(c[0])-int(b[0])==x and int(c[1])-int(b[1])==0 and int(d[0])-int(c[0])==0 and int(c[1])-int(d[1])==x :
     print('yes')
 else:
     print('no')
